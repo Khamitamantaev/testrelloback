771119dfcf93868222e6a88d9d5e59f30e7fb58f
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2024-08-15 21:23:43 +0500

    test add module prisma

--- app/src/auth/auth.module.ts
+++ app/src/auth/auth.module.ts
@@ -1,8 +1,21 @@
+//src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+export const jwtSecret = 'secret';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: jwtSecret,
+      signOptions: { expiresIn: '5m' }, // e.g. 30s, 7d, 24h
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
