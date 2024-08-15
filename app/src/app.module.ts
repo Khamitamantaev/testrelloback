a880e40d53f1519d86747f9f1f48cd906b5cbc42
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2024-08-15 21:27:22 +0500

    fix prisma module

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -3,11 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
