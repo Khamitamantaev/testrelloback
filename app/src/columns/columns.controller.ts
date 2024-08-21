52fa99bea028a27f4922b274dc46475cf8729699
Khamitamantaev <32989550+Khamitamantaev@users.noreply.github.com>
2024-08-21 17:58:04 +0500

    del console log

--- app/src/columns/columns.controller.ts
+++ app/src/columns/columns.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Req, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ColumnsService } from './columns.service';
 import { CreateColumnDto } from './dto/create-column.dto';
 import { UpdateColumnDto } from './dto/update-column.dto';
